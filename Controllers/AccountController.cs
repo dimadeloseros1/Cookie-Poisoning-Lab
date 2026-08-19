f00a2ed60d23891cd8329ab777ebfe0f1894b61c
Dima <dimadeloseros1@gmail.com>
2026-08-19 18:37:38 +1000

    Feat: Implement Login endpoint

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -11,5 +11,36 @@ public class AccountController : Controller
         return View();
     }
 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Login(LogginViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        string? role = null;
+
+        if (model.UserName == "user" && model.Password == "user")
+        {
+            role = "user";
+        }
+
+        if (model.UserName == "admin" && model.Password == "admin")
+        {
+            role = "admin";
+        }
+
+        if (role == null)
+        {
+            ModelState.AddModelError("", "Invalid username or password");
+        }
+
+        Response.Cookies.Append("username", model.UserName);
+        Response.Cookies.Append("role", role);
+
+        return RedirectToAction("Index", "Home");
+    }
     
 }
